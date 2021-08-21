b23ef60187825d0f8328df4da13c182a051c0775
md jahidul islam <za0162hid@gmail.com>
2021-08-21 22:30:13 +0600

    ignore file

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const fileUpload = require("express-fileupload");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7adfu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+// const port = 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -81,4 +82,5 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port);
+// app.listen(port);
+app.listen(port, () => console.log(`connected database server${port}`));
